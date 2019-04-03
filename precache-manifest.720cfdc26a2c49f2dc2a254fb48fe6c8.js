85e1584145cd66b046db91ec0683a1b01ca43c3a
Mateusz Michalak <michalakweb@gmail.com>
2019-04-03 15:06:01 +0200

    Updates

---
+++ precache-manifest.720cfdc26a2c49f2dc2a254fb48fe6c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f07bb8a5b81168292d66",
+    "url": "/calculator/static/css/main.b5df6de9.chunk.css"
+  },
+  {
+    "revision": "f07bb8a5b81168292d66",
+    "url": "/calculator/static/js/main.d5fbdb5b.chunk.js"
+  },
+  {
+    "revision": "0d721d06f78c96d55f57",
+    "url": "/calculator/static/js/runtime~main.19f1d22d.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/calculator/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "99ad6ca33ccf3d64ed6a5e28a61fa380",
+    "url": "/calculator/index.html"
+  }
+];
